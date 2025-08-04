52fc0622b5d610edc556d95088aa004efffa442a
vanshaj bansal <bnslgvansh67@gmail.com>
2025-08-04 22:46:19 +0530

    PrimeFactorization

--- August/Day5.java
+++ August/Day5.java
@@ -2,6 +2,20 @@ package August;
 
 import java.util.Scanner;
 
+class PrimeFactorization {
+    void factors(int n) {
+        for (int i = 2; i * i < n; i++) {
+            while (n % i == 0) {
+                n = n / i;
+                System.out.print(i + "x");
+            }
+            if (n != 1) {
+                System.out.print(n);
+            }
+        }
+    }
+}
+
 public class Day5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -20,5 +34,12 @@ public class Day5 {
         int lcm = (on1 * on2) / gcd;
         System.out.println("GCD ->" + gcd);
         System.out.println("LCM ->" + lcm);
+
+        System.out.println();
+        System.out.println("Enter the number to calculate the prime factorization;");
+        int num = sc.nextInt();
+        PrimeFactorization pf = new PrimeFactorization();
+        pf.factors(num);
+
     }
 }
