ec43c979a03fdda1f6959451181fa06987d81068
vanshaj bansal <bnslgvansh67@gmail.com>
2025-08-08 03:40:13 +0530

    Day 8 Completed

--- August/Day8.java
+++ August/Day8.java
@@ -142,43 +142,140 @@ class Pattern17 {
     }
 }
 
+class Pattern18 {
+    void Pattern(int n) {
+        int sp = 0;
+        int st = n;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sp; j++) {
+                System.out.print("\t");
+            }
+            for (int j = 1; j <= st; j++) {
+                if (i > 1 && i <= n / 2 && j > 1 && j < st) {
+                    System.out.print("\t");
+                } else {
+                    System.out.print("*\t");
+                }
+            }
+            if (i <= n / 2) {
+                sp++;
+                st = st - 2;
+            } else {
+                sp--;
+                st = st + 2;
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Pattern19 {
+    void Pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1) {
+                    if (j == n || j <= n / 2 + 1) {
+                        System.out.print("*\t");
+                    } else {
+                        System.out.print("\t");
+                    }
+                } else if (i <= n / 2) {
+                    if (j == n / 2 + 1 || j == n) {
+                        System.out.print("*\t");
+                    } else {
+                        System.out.print("\t");
+                    }
+                } else if (i == n / 2 + 1) {
+                    System.out.print("*\t");
+                } else if (i < n) {
+                    if (j == 1 || j == n / 2 + 1) {
+                        System.out.print("*\t");
+                    } else {
+                        System.out.print("\t");
+                    }
+                } else {
+                    if (j == 1 || j >= n / 2 + 1) {
+                        System.out.print("*\t");
+                    } else {
+                        System.out.print("\t");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Pattern20 {
+    void Pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j == 1 || j == n) {
+                    System.out.print("*\t");
+                } else if (i > n / 2 && (j == i || i + j == n + 1)) {
+                    System.out.print("*\t");
+                } else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+
+        }
+    }
+}
+
 public class Day8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // System.out.print("Enter the no. for Pattern 11;");
-        // int num11 = sc.nextInt();
-        // Pattern11 ptr11 = new Pattern11();
-        // ptr11.NumberPattern(num11);
-        // System.out.println();
-        // System.out.print("Enter the no. for Pattern 12;");
-        // int num12 = sc.nextInt();
-        // Pattern12 ptr12 = new Pattern12();
-        // ptr12.NumberPattern(num12);
-        // System.out.println();
-        // System.out.print("Enter the no. for Pattern 13;");
-        // int num13 = sc.nextInt();
-        // Pattern13 ptr13 = new Pattern13();
-        // ptr13.NumberPattern(num13);
-        // System.out.println();
-        // System.out.print("Enter the no. for Pattern 14;");
-        // int num14 = sc.nextInt();
-        // Pattern14 ptr14 = new Pattern14();
-        // ptr14.Table(num14);
-        // System.out.println();
-        // System.out.print("Enter the no. for Pattern 15;");
-        // int num15 = sc.nextInt();
-        // Pattern15 ptr15 = new Pattern15();
-        // ptr15.Diamond2(num15);
-        // System.out.println();
-        // System.out.print("Enter the no. for Pattern 16;");
-        // int num16 = sc.nextInt();
-        // Pattern16 ptr16 = new Pattern16();
-        // ptr16.Pattern(num16);
+        System.out.print("Enter the no. for Pattern 11;");
+        int num11 = sc.nextInt();
+        Pattern11 ptr11 = new Pattern11();
+        ptr11.NumberPattern(num11);
+        System.out.println();
+        System.out.print("Enter the no. for Pattern 12;");
+        int num12 = sc.nextInt();
+        Pattern12 ptr12 = new Pattern12();
+        ptr12.NumberPattern(num12);
+        System.out.println();
+        System.out.print("Enter the no. for Pattern 13;");
+        int num13 = sc.nextInt();
+        Pattern13 ptr13 = new Pattern13();
+        ptr13.NumberPattern(num13);
+        System.out.println();
+        System.out.print("Enter the no. for Pattern 14;");
+        int num14 = sc.nextInt();
+        Pattern14 ptr14 = new Pattern14();
+        ptr14.Table(num14);
+        System.out.println();
+        System.out.print("Enter the no. for Pattern 15;");
+        int num15 = sc.nextInt();
+        Pattern15 ptr15 = new Pattern15();
+        ptr15.Diamond2(num15);
+        System.out.println();
+        System.out.print("Enter the no. for Pattern 16;");
+        int num16 = sc.nextInt();
+        Pattern16 ptr16 = new Pattern16();
+        ptr16.Pattern(num16);
         System.out.println();
         System.out.print("Enter the no. for Pattern 17;");
         int num17 = sc.nextInt();
         Pattern17 ptr17 = new Pattern17();
         ptr17.Pattern(num17);
+        System.out.println();
+        System.out.print("Enter the no. for Pattern 18;");
+        int num18 = sc.nextInt();
+        Pattern18 ptr18 = new Pattern18();
+        ptr18.Pattern(num18);
+        System.out.println();
+        System.out.print("Enter the no. for Pattern 18;");
+        int num19 = sc.nextInt();
+        Pattern19 ptr19 = new Pattern19();
+        ptr19.Pattern(num19);
+        System.out.println();
+        System.out.print("Enter the no. for Pattern 18;");
+        int num20 = sc.nextInt();
+        Pattern20 ptr20 = new Pattern20();
+        ptr20.Pattern(num20);
 
     }
 }
