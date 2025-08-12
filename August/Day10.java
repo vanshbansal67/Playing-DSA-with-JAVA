98387a1a6f31e21fa7219756c7b2d3f33250e786
vanshaj bansal <bnslgvansh67@gmail.com>
2025-08-12 14:55:52 +0530

    Day 10 completed

--- August/Day10.java
+++ August/Day10.java
@@ -6,6 +6,10 @@ import java.util.Scanner;
 // Any Base to Decimal
 // Any Base to Any Base
 
+// Addition in  Base
+// Substraction in Base
+// Multiplication in Base
+
 class DecimalToAnyBase {
     void DtoAnyBase(int n, int b) {
         int rv = 1;
@@ -48,6 +52,104 @@ class AnyBaseToAnyBase {
     }
 }
 
+class AdditionInBase {
+    void Addition(int b, int n1, int n2) {
+        int rv = 0;
+        int c = 0;
+        int p = 1;
+        while (n1 > 0 || n2 > 0 || c > 0) {
+            int d1 = n1 % 10;
+            int d2 = n2 % 10;
+            n1 = n1 / 10;
+            n2 = n2 / 10;
+            int s = d1 + d2 + c;
+            c = s / b;
+            s = s % b;
+            rv += s * p;
+            p = p * 10;
+        }
+        System.out.println(rv);
+    }
+}
+
+class MultipicationInBase {
+    void Multiplication(int b, int n1, int n2) {
+        int rv = 0;
+        int p = 1;
+        while (n2 > 0) {
+            int d2 = n2 % 10;
+            n2 = n2 / 10;
+            int sprd = getProductWithASingleDigit(b, n1, d2);
+            rv = getSum(b, rv, sprd * p);
+            p = p * 10;
+        }
+        System.out.println(rv);
+    }
+
+    public static int getProductWithASingleDigit(int b, int n1, int d2) {
+        int rv = 0;
+        int c = 0;
+        int p = 1;
+        while (n1 > 0 || c > 0) {
+            int d1 = n1 % 10;
+
+            n1 = n1 / 10;
+
+            int s = d2 * d1 + c;
+            c = s / b;
+            s = s % b;
+            rv += s * p;
+            p = p * 10;
+        }
+        return rv;
+    }
+
+    public static int getSum(int b, int n1, int n2) {
+        int rv = 0;
+        int c = 0;
+        int p = 1;
+        while (n1 > 0 || n2 > 0 || c > 0) {
+            int d1 = n1 % 10;
+            int d2 = n2 % 10;
+            n1 = n1 / 10;
+            n2 = n2 / 10;
+            int s = d1 + d2 + c;
+            c = s / b;
+            s = s % b;
+            rv += s * p;
+            p = p * 10;
+        }
+        return rv;
+    }
+}
+
+class getSubstraction {
+    void Substraction(int b, int n1, int n2) {
+        int rv = 0;
+        int p = 1;
+        int c = 0;
+        while (n2 > 0) {
+            int d1 = n1 % 10;
+            n1 = n1 / 10;
+            int d2 = n2 % 10;
+            n2 = n2 / 10;
+
+            int d = 0;
+            d2 = d2 + c;
+            if (d2 >= d1) {
+                c = 0;
+                d = d2 - d1;
+            } else {
+                c = -1;
+                d = d2 + b - d1;
+            }
+            rv = rv + d * p;
+            p = p * 10;
+        }
+        System.out.println(rv);
+    }
+}
+
 public class Day10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -67,13 +169,37 @@ public class Day10 {
         System.out.println();
         System.out.print("Enter the base crom which to convert the number:");
         int num5 = sc.nextInt();
-
         System.out.print("Enter the number to convert it to any other base:");
         int num6 = sc.nextInt();
-
         System.out.print("Enter the base to convert it:");
         int num7 = sc.nextInt();
         AnyBaseToAnyBase abtab = new AnyBaseToAnyBase();
         abtab.ABtoAB(num5, num6, num7);
+        System.out.print("Enter the base of numbers:");
+        int num8 = sc.nextInt();
+        System.out.print("Enter the first number:");
+        int num9 = sc.nextInt();
+        System.out.print("Enter the second number:");
+        int num10 = sc.nextInt();
+        AdditionInBase ADB = new AdditionInBase();
+        ADB.Addition(num8, num9, num10);
+        System.out.println();
+        System.out.print("Enter the base of numbers:");
+        int num11 = sc.nextInt();
+        System.out.print("Enter the first number:");
+        int num12 = sc.nextInt();
+        System.out.print("Enter the second number:");
+        int num13 = sc.nextInt();
+        MultipicationInBase MLB = new MultipicationInBase();
+        MLB.Multiplication(num11, num12, num13);
+        System.out.println();
+        System.out.print("Enter the base of numbers:");
+        int num14 = sc.nextInt();
+        System.out.print("Enter the first number:");
+        int num15 = sc.nextInt();
+        System.out.print("Enter the second number:");
+        int num16 = sc.nextInt();
+        getSubstraction MLB = new getSubstraction();
+        MLB.Substraction(num14, num15, num16);
     }
 }
